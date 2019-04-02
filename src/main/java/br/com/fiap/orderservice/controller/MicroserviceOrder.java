243643820e5105eaa0e9650e6c7ced8260dd7e1e
crcarvalho <c_r_carvalho@hotmail.com>
2019-04-01 18:45:07 -0700

    Handler Error Exception and Tests

--- src/main/java/br/com/fiap/orderservice/controller/MicroserviceOrder.java
+++ src/main/java/br/com/fiap/orderservice/controller/MicroserviceOrder.java
@@ -3,12 +3,16 @@ package br.com.fiap.orderservice.controller;
 import br.com.fiap.orderservice.dto.OrderDTO;
 import br.com.fiap.orderservice.dto.OrderItemDTO;
 import br.com.fiap.orderservice.dto.TransacaoPagamentoDTO;
+import br.com.fiap.orderservice.exception.OrderInvalidException;
+import br.com.fiap.orderservice.exception.OrderNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.HashMap;
 
 @Slf4j
@@ -23,7 +27,7 @@ public class MicroserviceOrder {
         System.out.println("**** ID PEDIDO: "+ idPedido);
         OrderDTO order = new OrderDTO();
         ResponseEntity res;
-
+/*
         try{
             if( mapOrders.containsKey(idPedido)){
                  order = mapOrders.get(idPedido);
@@ -31,7 +35,12 @@ public class MicroserviceOrder {
             } else res = new ResponseEntity(null, HttpStatus.NOT_FOUND);
         }catch(Exception e){
             res = new ResponseEntity(null, HttpStatus.NOT_FOUND);
-        }
+        }*/
+
+        if( mapOrders.containsKey(idPedido)){
+            order = mapOrders.get(idPedido);
+            res = new ResponseEntity(order, HttpStatus.OK);
+        }else throw new OrderNotFoundException("Não foi possivel localizar o pedido "+ idPedido +".");
 
         return res;
     }
@@ -62,47 +71,50 @@ public class MicroserviceOrder {
     }
 
     @PutMapping("/order/update/{id}")
-    public String atualizarPedido(@PathVariable (value = "id", required = true) String id,
+    public ResponseEntity<String> atualizarPedido(@PathVariable (value = "id", required = true) String id,
                                   @RequestBody OrderDTO aOrder){
-
+        ResponseEntity res;
         String url = "http://localhost:8080/order/findById/";
         OrderDTO order = new OrderDTO();
-        try{
-            if( id == null || id.equals("") )
-                throw new Exception("Favor informar o Id do registro para alteração.");
+        if( id == null || id.equals("") )
+            throw new OrderInvalidException("Favor informar o Id do registro para alteração.");
 
-            order = mapOrders.get( id );
-            if( order == null ) throw new Exception("Pedido não encontrado!");
+        //try{
+        order = mapOrders.get( id );
+        if( order == null ) throw new OrderNotFoundException("Pedido não encontrado!");
 
-            //Atualiza atributos do order
-            order.setShippingAddress(aOrder.getShippingAddress());
-            order.setQuantidadeItens(aOrder.getQuantidadeItens());
-            order.setPrecoTotal(new BigDecimal(aOrder.getPrecoTotal()));
-            order.setFormaPagamento(aOrder.getFormaPagamento());
-            //Atualiza atributos do OrderItem
-            for(OrderItemDTO item : order.getLstItensPedido()) {
-                for( OrderItemDTO aItem : aOrder.getLstItensPedido()) {
-                    if( item.getDecricao().equalsIgnoreCase(aItem.getDecricao())) {
-                        item.setDecricao(aItem.getDecricao());
-                        item.setPrecoTotal(new BigDecimal(aItem.getPrecoTotal()));
-                        item.setPrecoUnitario( new BigDecimal(aItem.getPrecoUnitario()));
-                        item.setQuantidade(aItem.getQuantidade());
-                    }
+        //Atualiza atributos do order
+        order.setShippingAddress(aOrder.getShippingAddress());
+        order.setQuantidadeItens(aOrder.getQuantidadeItens());
+        order.setPrecoTotal(new BigDecimal(aOrder.getPrecoTotal()));
+        order.setFormaPagamento(aOrder.getFormaPagamento());
+        //Atualiza atributos do OrderItem
+        for(OrderItemDTO item : order.getLstItensPedido()) {
+            for( OrderItemDTO aItem : aOrder.getLstItensPedido()) {
+                if( item.getDecricao().equalsIgnoreCase(aItem.getDecricao())) {
+                    item.setDecricao(aItem.getDecricao());
+                    item.setPrecoTotal(new BigDecimal(aItem.getPrecoTotal()));
+                    item.setPrecoUnitario( new BigDecimal(aItem.getPrecoUnitario()));
+                    item.setQuantidade(aItem.getQuantidade());
                 }
             }
-            //Atualiza atributos
-            TransacaoPagamentoDTO pgto = order.getTransacao();
-            TransacaoPagamentoDTO pgtoNovo = order.getTransacao();
-            pgto.setBandeira(pgtoNovo.getBandeira());
-            pgto.setDataValidadeCartao(pgtoNovo.getDataValidadeCartao());
-            pgto.setId(pgtoNovo.getId());
-            pgto.setNumeroCartao(pgtoNovo.getNumeroCartao());
-        }catch(Exception e){
-            System.out.println("Exceção: "+e.getMessage());
-            url += null;
         }
+        //Atualiza atributos
+        TransacaoPagamentoDTO pgto = order.getTransacao();
+        TransacaoPagamentoDTO pgtoNovo = order.getTransacao();
+        pgto.setBandeira(pgtoNovo.getBandeira());
+        pgto.setDataValidadeCartao(pgtoNovo.getDataValidadeCartao());
+        pgto.setId(pgtoNovo.getId());
+        pgto.setNumeroCartao(pgtoNovo.getNumeroCartao());
+        url += order.getIdOrder();
+        res =  new ResponseEntity(url, HttpStatus.OK);
+        /*}catch(Exception e){
+            url += null;
+            res =  new ResponseEntity(url, HttpStatus.NOT_FOUND);
+            throw new OrderInvalidException("Ocorreu um erro ao atualizar o registro.");
+        }*/
 
-        return url;
+        return res;
     }
 
     @DeleteMapping("/order/delete/{id}")
